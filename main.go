2f3d278581d44cc599f54b3e0904d2e8ed118e78
Fredrik Ragnarsson <fredrik.ragnarsson@gmail.com>
2019-04-13 17:00:21 +0200

    Removed output

--- main.go
+++ main.go
@@ -10,10 +10,7 @@ package main
 
 import (
 	"flag"
-	"fmt"
-
-	"github.com/subfinder/subfinder/libsubfinder/helper"
-	"github.com/subfinder/subfinder/subf"
+	"github.com/MrRagnar/subfinder/subf"
 )
 
 // ParseCmdLine ...  Parses command line arguments into a setting structure
@@ -52,16 +49,6 @@ func ParseCmdLine() (s *subf.Subfinder) {
 func main() {
 
 	subfinder := ParseCmdLine()
-
-	if !subfinder.State.Silent {
-		fmt.Println("===============================================")
-		fmt.Printf("%s%s-=Subfinder%s v1.1.3 github.com/subfinder/subfinder\n", helper.Info, helper.Cyan, helper.Reset)
-		fmt.Println("===============================================")
-	}
-
 	subfinder.Init()
-
 	_ = subfinder.PassiveEnumeration()
-
-	fmt.Printf("\n")
 }
